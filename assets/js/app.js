127ca5fca6d805073db8a27698a2cfe9d3b8485d
Mestniy183 <ken-zero@ya.ru>
2026-04-22 11:58:05 +0300

    new-artifacts

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+        renderFraction: function (currentClass, totalClass) {
+            return '<span class="' + currentClass + '"></span>' +
+            ' из ' +
+            '<span class="' + totalClass + '"></span>';
+        }
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    },
+})
